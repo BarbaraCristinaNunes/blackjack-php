8e31465e52a6ce5e62e90719e48c2b3348f528f3
BarbaraCristinaNunes <90681011+BarbaraCristinaNunes@users.noreply.github.com>
2021-10-28 15:31:17 +0200

    finished the step 5, 6 and 7

--- Player.php
+++ Player.php
@@ -1,9 +1,16 @@
 <?php
 
-class player
+class Player
 {
     private $cards = [];
     private $lost = false;
+
+    public function __construct(Deck $deck)
+    {
+        // draw to cards from the deck an put then in this cards
+        array_push($this->cards,$deck->drawCard());
+        array_push($this->cards,$deck->drawCard());        
+    }
 
     public function hit(){
 
--- blackjack.php
+++ blackjack.php
@@ -1,21 +1,33 @@
 <?php
-class blackjack 
+class Blackjack 
 {
     private $player;
     private $dealer;
     private $deck;
 
+    public function __construct()
+    {
+        $this->deck = new Deck();
+        $this->deck->shuffle();        
+        $this->player = new Player($this->deck);
+        $this->dealer = new Player($this->deck);
+        
+    }
+
+
     public function getPlayer(){
+        return $player;
 
     }
 
     public function getDealer(){
-
+        return $dealer;
     }
 
     public function getDeck(){
-
+        return $deck;
     }
+    
 }
 
 ?>
